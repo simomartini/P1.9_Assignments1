3f994d80d72f914771f181f7434ade7348364f1b
Simone Martini <simomartini@MacBook-di-Simone-2.local>
2017-02-07 12:11:16 +0100

    new irrational functions

--- exe1.py
+++ exe1.py
@@ -6,7 +6,7 @@ from pylab import *
 
 #print("Give me a Number for the function as input:\n")
 if len(sys.argv)==1:
-    print("USAGE:\n 1: f(x)=x;\n")
+    print("USAGE:\n 1: f(x)=x;\n 2: f(x)=exp(x);\n 3: f(x)=sqrt(|x|)")
     sys.exit()
 else:
     f=int(sys.argv[1])
@@ -19,6 +19,10 @@ xval=arange(start,stop,step)
 
 if f==1:
     yval=xval
+elif f==2:
+    yval=exp(xval)
+elif f==3:
+    yval=sqrt(abs(xval))
 
 plot(xval,yval)
 show()
